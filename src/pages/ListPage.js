d037321b6ed0942b605d4e1ebc8b81389e4d456c
buraksvk <50809470+buraksvk@users.noreply.github.com>
2022-06-14 17:17:38 +0300

    filter options and types added

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { ArrowDownUp, ChevronDown } from 'react-bootstrap-icons';
 function ListPage() {
     const [list, setList] = useState([{ hotel: "Voyage Hotel", rating: 9.7, time: 1655146561 }, { hotel: "Maxx Royal Hotel", rating: 8.3, time: 1655146606 }, { hotel: "Vogue Hotel", rating: 7.5, time: 1655147906 }]);
+    const [open, setOpen] = useState(false)
+    const [filterBy, setFilterBy] = useState("up")
 
     const upRate = (index) => {
         let arr = list[index];
@@ -25,6 +27,11 @@ function ListPage() {
         }
     }
 
+    const selectFilter = (type) => {
+        setFilterBy(type);
+        setOpen(false)
+    }
+
     function mapper(array) {
         var str = "[";
         for (let i = 0; i < array.length; i++) {
@@ -44,7 +51,7 @@ function ListPage() {
                 <button className='add-hotel-button'>+</button>
                 <h1 className='title'>OTEL EKLE</h1>
             </div>
-            <div className='filter-button space-between'>
+            <div className='filter-button space-between' onClick={() => { setOpen(!open) }}>
                 <span>
                     <ArrowDownUp className='icon' />
                     <span style={{ marginLeft: "7px" }}>Sıralama</span>
@@ -53,33 +60,42 @@ function ListPage() {
                     <ChevronDown color="#aaa" className='icon' />
                 </span>
             </div>
-
             {
-                list.map((item, index) => {
-                    return (
-                        <div className='hotel-box' key={"hotel-" + index}>
-                            <div className='row'>
-                                <div className='hotel-image'>
-                                    <img className="image" src="image/hotel-image.png" alt="hotel-image" />
-                                </div>
-                                <div className='hotel-content-col'>
-                                    <p className='hotel-title'>{item.hotel}</p>
-                                    <div className="hotel-rating-box">
-                                        <p className='hotel-rating'>{item.rating} Puan
-                                        </p>
+                open ? 
+                <div className='selection-box'>
+                    <div className='option' style={filterBy === "up" ? {backgroundColor:"#4894fd"}:{}} onClick={() => {selectFilter("up")}}>{"Puan (Artan)"}</div>
+                    <div className='option' style={filterBy === "down" ? {backgroundColor:"#4894fd"}:{}} onClick={() => {selectFilter("down")}}>{"Puan (Azalan)"}</div>
+                </div>
+                :null
+            }
+            <div className='hotel-list'>
+                {
+                    list.map((item, index) => {
+                        return (
+                            <div className='hotel-box' key={"hotel-" + index}>
+                                <div className='row'>
+                                    <div className='hotel-image'>
+                                        <img className="image" src="image/hotel-image.png" alt="hotel-image" />
                                     </div>
-                                    <div className='hotel-rating-button'>
-                                        <button onClick={() => { upRate(index) }}>puan artır</button>
-                                        <button onClick={() => { downRate(index) }}>puan azalt</button>
+                                    <div className='hotel-content-col'>
+                                        <p className='hotel-title'>{item.hotel}</p>
+                                        <div className="hotel-rating-box">
+                                            <p className='hotel-rating'>{item.rating} Puan
+                                            </p>
+                                        </div>
+                                        <div className='hotel-rating-button'>
+                                            <button onClick={() => { upRate(index) }}>puan artır</button>
+                                            <button onClick={() => { downRate(index) }}>puan azalt</button>
+                                        </div>
                                     </div>
                                 </div>
+
                             </div>
 
-                        </div>
-
-                    )
-                })
-            }
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
